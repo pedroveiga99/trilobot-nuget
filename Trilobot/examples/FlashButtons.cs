25283cb5626cb91f8b93c244aeca014c65afd154
Pedro Veiga <p.veiga@campus.fct.unl.pt>
2024-01-10 23:08:33 +0000

    add flash button example

---
+++ Trilobot/examples/FlashButtons.cs
@@ -0,0 +1,31 @@
+﻿using Trilobot;
+
+namespace TrilobotExamples;
+public static class FlashButtons
+{
+    public static async Task Run(TrilobotController trilobot)
+    {
+        string help = """"
+            Shows how to turn the button LEDs on and off, by having them flash.
+            It will print if the button is pressed, double pressed and hold.
+            """";
+        Console.WriteLine(help);
+
+        string exitHelp = "Press \"e\" to exit this example.";
+        Console.WriteLine(exitHelp);
+
+        int flashes = 10;  // How many times to flash the LEDs
+        TimeSpan interval = TimeSpan.FromSeconds(0.3);  // Control the speed of the LED animation
+
+        for (int i = 0; i < flashes; i++)
+        {
+            Console.WriteLine($"Flash {i}");
+
+            trilobot.Buttons.SetAllButtonLeds(true);
+
+            await Task.Delay(interval);
+
+            trilobot.Buttons.SetAllButtonLeds(false);
+        }
+    }
+}
--- Trilobot/examples/Program.cs
+++ Trilobot/examples/Program.cs
@@ -4,22 +4,36 @@ namespace TrilobotExamples;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         TrilobotController trilobot = new();
+        bool running = true;
 
         string help = """"
-            mb => Buttons lister example
+            mb => Buttons lister
+            fb => Flash buttons LEDs
+
+            exit => Exit Program
             """";
-        Console.WriteLine(help);
 
-        string? a = Console.ReadLine();
-
-        switch (a)
+        while (running)
         {
-            case "mb":
-                MultipleButtons.Run(trilobot);
-                break;
+            Console.WriteLine(help);
+
+            string? example = Console.ReadLine();
+
+            switch (example)
+            {
+                case "exit":
+                    running = false;
+                    break;
+                case "mb":
+                    MultipleButtons.Run(trilobot);
+                    break;
+                case "fb":
+                    await FlashButtons.Run(trilobot);
+                    break;
+            }
         }
     }
 }
